ef5d694ae9d09e1a74b091f169d6812d16b3e727
karatikidd94 <alexazevedo9494@gmail.com>
2022-07-12 23:06:27 -0700

    Added Styling to recipe/index.html

--- main_app/views.py
+++ main_app/views.py
@@ -3,15 +3,17 @@ from .models import Recipe
 from django.http import HttpResponse
 
 class Recipe:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, meal, title, length, ingredients, prep):
-    self.meal = meal
-    self.title = title
-    self.length = length
-    self.ingredients = ingredients
-    self.prep = prep
+    def __init__(self, title, meal, length, ingredients, directions):
+        self.title = title
+        self.meal = meal
+        self.length = length
+        self.ingredients = ingredients
+        self.directions = directions
+
 
 recipes = [
-  Recipe('Lunch', 'Grilled Potato Salad', '45mins', '2lb Potatoes, 1/4 cup olive oil, salt & pepper to taste, 3 tbsp vinegar, 1 tbsp mustard, 1 tsp celery seeds, 1 tsp sugar, 2 tsp chopped herbs', 'mix it all together'),
+  Recipe('Grilled Potato Salad', 'Lunch', '45 mins', '2lb Potatoes, 1/4 cup olive oil, salt & pepper to taste, 3 tbsp vinegar, 1 tbsp mustard, 1 tsp celery seeds, 1 tsp sugar, 2 tsp chopped herbs', 'mix it all together'),
+  Recipe('Spinach & Egg Tacos', 'Breakfest', '10 mins', '1/4 avocado, 1 tsp lime juice, Pinch of Salt, 2 chopped hard-boiled eggs, 2 corn tortillas, 1 cup chopped spinach, 2 tbsp shredded cheddar, 2 tbsp salsa', 'Smash avocado in a small bowl with lime juice and salt. Mix in eggs. Divide the mixture between tortillas and top each with 1/2 cup spinach and 1 tablespoon each cheese and salsa.')
 ]
 
 # Create your views here.
